4e9d2bc95fe94f6e0e5195fa8e9daf2f220e5804
jansk32 <44828185+jansk32@users.noreply.github.com>
2021-10-25 21:39:42 +1100

    basic display of artwork obj on app page and fetching data

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import ArtWorkObj from "./artworkObj";
+import {useState, useEffect} from 'react'
+import axios from 'axios';
 
 function App() {
+
+  var [arts, setArts] = useState([]);
+  var [isLoading, setIsLoading] = useState(true)
+
+
+  useEffect(() => {
+    fetch("http:localhost:8080/")
+    .then((data) => {
+      setArts([11922,
+        2032,
+        816522]);
+      setIsLoading(false)
+      console.log(data)
+    })
+    .catch((err) => console.error(err))
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (isLoading &&  <div className="App">
+      {arts.map((obj) => <ArtWorkObj artwork={obj} />
+      )}
+    </div>)
   );
 }
 
--- client/src/artworkObj.js
+++ client/src/artworkObj.js
@@ -1,7 +1,13 @@
 import React, {useState, useEffect} from 'react'
 
 function ArtWorkObj(props) {
-    var [artData, setArtData] = useState(props)
+    var [artData, setArtData] = useState({})
+
+    useEffect(() => {
+        fetch("http://localhost:8080/" + props.artwork)
+        .then((data) => setArtData(data))
+        .catch((err) => console.error(err))
+    })
 
     return(
         <div>
