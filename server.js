0571bc5e3858576a68f30b31ae5f102b8a97f4a3
jansk32 <44828185+jansk32@users.noreply.github.com>
2021-10-25 20:58:19 +1100

    get sample data from server

--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const axios = require('axios');
 
 var app = express();
 
+var objIds = []
+
+// get ids with primary image 
+app.get("/", function (req,res) {
+    axios("https://collectionapi.metmuseum.org/public/collection/v1/search?q=sunflowers")
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err))
+
+})
+
+app.get("/arts", function(req,res) {
+
+})
+
 var port = process.env.PORT || 8080;
 
 app.listen(port, () => {
